0cd95a5994397d2d61860a7c40b0f898d2d881a5
Kyle Bocinsky <bocinsky@gmail.com>
2021-07-27 16:23:36 -0600

    initial script and data

---
+++ prepare-nlcd.sh
@@ -0,0 +1,8 @@
+cd NLCD/raw
+rm -r */
+xargs -n 1 -P 0 curl -C - -O -s < ../../nlcd-downloads.txt
+
+find . -type f -name '*.zip' -print | parallel -j+0 "unzip -qq {} -d {.} && gdal_translate -q {.}/{.}.img ../cogs/{.}.tif -of COG -co COMPRESS=DEFLATE && rm -r {.}"
+
+rm -r */
+cd ../../
