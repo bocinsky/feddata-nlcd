6e9e1cd9820797962183cd244277bcc2951db64f
Kyle Bocinsky <bocinsky@gmail.com>
2021-09-07 12:41:07 -0600

    added L48 Tree Canopy data

--- prepare-nlcd.sh
+++ prepare-nlcd.sh
@@ -1,5 +1,6 @@
 cd NLCD/raw
 rm -r */
+rm -rf ../cogs/*
 xargs -n 1 -P 0 curl -C - -O -s < ../../nlcd-downloads.txt
 
 find . -type f -name '*.zip' -print | parallel -j+0 "unzip -qq {} -d {.} && gdal_translate -q {.}/{.}.img ../cogs/{.}.tif -of COG -co COMPRESS=DEFLATE && rm -r {.}"
